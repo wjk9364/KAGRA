98dac810f6b4ec58e3fb5dcdca03843e66b19f65
wjk9364 <wjk9364@unist.ac.kr>
2023-10-20 03:57:24 +0900

    Update tnf.py

--- tnf.py
+++ tnf.py
@@ -5,36 +5,17 @@ g = open("ny.txt","w")
 f = open("256ch.txt",'r')
 while True:
  line = f.readline().replace('\n','')
- path = "/data/kagra/home/kihyun.jung/silentrun/K1/"+line
+ path = #path
  if not line: break
  if int(len(os.listdir(path))) != 171:
   g.write(line+str(len(os.listdir(path)))+"\n")
 f.close
 g.write("\n")
-"""
-f = open("512ch.txt",'r')
-while True:
- line = f.readline().replace('\n','')
- path = "/data/kagra/home/kihyun.jung/silentrun/K1/"+line
- if not line: break
- if int(len(os.listdir(path))) != 171:
-  g.write(line+str(len(os.listdir(path)))+"\n")
-f.close
-g.write("\n")
-f = open("1024ch.txt",'r')
-while True:
- line = f.readline().replace('\n','')
- path = "/data/kagra/home/kihyun.jung/silentrun/K1/"+line
- if not line: break
- if int(len(os.listdir(path))) != 171:
-  g.write(line+str(len(os.listdir(path)))+"\n")
-f.close
-g.write("\n")
-"""
+
 f = open("2048ch.txt",'r')
 while True:
  line = f.readline().replace('\n','')
- path = "/data/kagra/home/kihyun.jung/silentrun/K1/"+line
+ path = 
  if not line: break
  if int(len(os.listdir(path))) != 171:
   g.write(line+str(len(os.listdir(path)))+"\n")
@@ -43,7 +24,7 @@ g.write("\n")
 f = open("4096ch.txt",'r')
 while True:
  line = f.readline().replace('\n','')
- path = "/data/kagra/home/kihyun.jung/silentrun/K1/"+line
+ path = 
  if not line: break
  if int(len(os.listdir(path))) != 171:
   g.write(line+str(len(os.listdir(path)))+"\n")
@@ -52,7 +33,7 @@ g.write("\n")
 f = open("16384ch.txt",'r')
 while True:
  line = f.readline().replace('\n','')
- path = "/data/kagra/home/kihyun.jung/silentrun/K1/"+line
+ path = 
  if not line: break
  if int(len(os.listdir(path))) != 171:
   g.write(line+str(len(os.listdir(path)))+"\n")
